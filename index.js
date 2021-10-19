3337c6ea5ef4f7f7cc1a5219b197b9b56541491c
Bagas <priambudibagaskara@gmail.com>
2021-10-19 20:22:16 +0700

    Added a Procfile 2.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+var cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(cors());
+
+const port = process.env.PORT || 3001;
 const handleServer = () => console.log(`App running at port ${port}`);
 app.get("/", (req, res) => {
-  res.send(200);
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ a: 1 }, null, 3));
 });
 
 app.listen(port, handleServer);
